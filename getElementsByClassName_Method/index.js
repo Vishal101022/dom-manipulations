de90665b20955df8fa09ec98dc4617a819d7a832
Vishal101022 <raivishal939@gmail.com>
2024-09-12 07:29:06 +0530

    Add Dom assignment

---
+++ getElementsByClassName_Method/index.js
@@ -0,0 +1,7 @@
+// Write your code below:
+const list = document.getElementsByClassName("fruits");
+list[0].children[0].style.fontWeight = "bold";
+list[0].children[1].style.fontWeight = "bold";
+list[0].children[2].style.fontWeight = "bold";
+list[0].children[3].style.fontWeight = "bold";
+list[0].children[2].style.background = "yellow";
