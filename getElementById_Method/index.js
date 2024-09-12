a6dbb469e66606ebf7a58f91643b6a04231fd4a9
Vishal101022 <raivishal939@gmail.com>
2024-09-12 07:18:57 +0530

    Add DOM assignment

---
+++ getElementById_Method/index.js
@@ -0,0 +1,14 @@
+// Write your code below:
+const heading = document.getElementById("main-heading");
+heading.textContent = "Fruit World";
+heading.style.color = "orange";
+
+const header = document.getElementById("header");
+header.style.backgroundColor = "green";
+header.style.borderBottom = "3px solid orange";
+
+const subHeader = document.getElementById("basket-heading");
+subHeader.style.color = "green";
+
+const greet = document.getElementById("thanks");
+greet.innerHTML = "<p>Please visit us again</p>";
