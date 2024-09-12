66d4c3946dff4dabd6528fc2b468e23d5450ac73
Vishal101022 <raivishal939@gmail.com>
2024-09-12 07:34:36 +0530

    Add Dom assignment

---
+++ getElementsByTagName_Method/index.js
@@ -0,0 +1,8 @@
+// Write your code below:
+const list = document.getElementsByTagName("li");
+
+for (let i = 0; i < list.length; i++) {
+  list[i].style.fontStyle = "italic";
+}
+
+list[list.length - 1].style.color = "blue";
