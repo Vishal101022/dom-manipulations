4cb60475cd577659e64f8fbb9a354061585302ae
Vishal101022 <raivishal939@gmail.com>
2024-09-14 11:33:03 +0530

    Add handleEdit fn

--- Having_Edit_Functionality/index.js
+++ Having_Edit_Functionality/index.js
@@ -16,17 +16,34 @@ function handleFormSubmit(event) {
 
   let list = document.createElement("li");
   list.textContent = `${userName} - ${userEmail} - ${userPhone}`;
+  // create delete btn
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "delete";
   deleteButton.classList.add("delete-btn");
   list.appendChild(deleteButton);
   deleteButton.addEventListener("click", handleDelete);
+  // create edit btn
+  const editButton = document.createElement("button");
+  editButton.textContent = "edit";
+  editButton.classList.add("edit-btn");
+  list.appendChild(editButton);
+  editButton.addEventListener("click", handleEdit);
 
   const unorderList = document.querySelector("ul");
   unorderList.appendChild(list);
 
   function handleDelete(e) {
     const li = e.target.parentElement;
+    unorderList.removeChild(li);
+    localStorage.removeItem(userEmail);
+  }
+
+  function handleEdit(e) {
+    const li = e.target.parentElement;
+    const user = JSON.parse(localStorage.getItem(userEmail));
+    document.querySelector("#username").value = user.username;
+    document.querySelector("#email").value = user.email;
+    document.querySelector("#phone").value = user.phone;
     unorderList.removeChild(li);
     localStorage.removeItem(userEmail);
   }
